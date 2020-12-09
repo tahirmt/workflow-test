b165dd54ec6a549a411c0445362f95f551b5ed2d
Mahmood Tahir <mahmood.tahir@thescore.com>
2020-12-09 08:51:52 -0500

    New project

--- Sources/TSCoreKitTests/TSCoreKitTests.swift
+++ Sources/TSCoreKitTests/TSCoreKitTests.swift
@@ -1,14 +1,14 @@
 //
-//  TestTests.swift
-//  TestTests
+//  TSCoreKitTests.swift
+//  TSCoreKitTests
 //
 //  Created by Mahmood Tahir on 2020-12-09.
 //
 
 import XCTest
-@testable import Test
+@testable import TSCoreKit
 
-class TestTests: XCTestCase {
+class TSCoreKitTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- Sources/TSCoreKitUITests/TSCoreKitUITests.swift
+++ Sources/TSCoreKitUITests/TSCoreKitUITests.swift
@@ -1,13 +1,13 @@
 //
-//  TestUITests.swift
-//  TestUITests
+//  TSCoreKitUITests.swift
+//  TSCoreKitUITests
 //
 //  Created by Mahmood Tahir on 2020-12-09.
 //
 
 import XCTest
 
-class TestUITests: XCTestCase {
+class TSCoreKitUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
